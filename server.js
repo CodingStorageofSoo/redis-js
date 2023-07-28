4f9aff7cf51ca2abace1fa6a8023b284ac76c18b
CodingStorageSoo <chlqudtn347@gmail.com>
2023-07-28 16:47:01 -0700

    7/28

--- server.js
+++ server.js
@@ -1,73 +1,79 @@
 // npm init -y
-// npm i ioredis
+// npm i express
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-// redis-server
-// redis-cli
-// PORT 6379
-
-const Redis = require("ioredis");
-const redisClient = new Redis(); // Assuming Redis server is running locally
-
+// npm i axios
 const axios = require("axios");
 
-redisClient.set("count", 0);
+// npm i ioredis
+// redis-server
+// redis-cli
+const Redis = require("ioredis");
+const redisClient = new Redis();
 
-async function makePostRequest(url, data) {
+async function getBidResponse(url, data) {
   try {
-    const response = await axios.get(url);
-    await redisClient.incr("count");
-    const countValue = await redisClient.get("count");
-    console.log(redisClient.get("count"));
-    return [response.data, countValue];
+    const response = await axios.get(url, { params: data });
+
+    // Prevent Race Condition using Redis
+    const cnt = await redisClient.incr("cnt");
+
+    redisClient
+      .multi()
+      .zadd(
+        "bidResponses",
+        response.data.price * 1000 - cnt,
+        JSON.stringify({
+          id: response.data.id,
+          price: response.data.price,
+          order: cnt,
+        })
+      )
+      .exec((err, results) => {
+        if (err) {
+          console.error("Redis Error:", err);
+          return;
+        }
+      });
   } catch (error) {
-    console.error("Error making POST request:", error.message);
+    console.error("Error making GET request:", error.message);
   }
 }
 
-async function main() {
-  const urlList = [
-    "http://localhost:3001/processBid",
-    "http://localhost:3002/processBid",
-    "http://localhost:3003/processBid",
-  ];
-  const promises = urlList.map((url) => makePostRequest(url, {}));
+app.post("/bidRequest", async (req, res) => {
+  redisClient
+    .multi()
+    .del("bidResponses")
+    .set("cnt", 0)
+    .exec(async () => {
+      const urlList = [
+        "http://localhost:3001/processBid",
+        "http://localhost:3002/processBid",
+        "http://localhost:3003/processBid",
+      ];
 
-  try {
-    const responses = await Promise.all(promises);
-    responses.forEach((response) => {
-      console.log(response);
+      try {
+        //  Send bid Request Simultaneously
+        await Promise.all(urlList.map((url) => getBidResponse(url, {})));
+
+        const sortedData = await redisClient
+          .zrevrange("bidResponses", 0, -1)
+          .then((result) => {
+            return result.map((data) => JSON.parse(data));
+          });
+
+        console.log(sortedData);
+        res.json(sortedData);
+      } catch (error) {
+        console.error("Error in handling bid request:", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
     });
-  } catch (error) {
-    console.error("Error in making POST requests:", error.message);
-  } finally {
-    // Close the Redis connection when done
-    redisClient.quit();
-  }
-}
-
-main().catch((err) => {
-  console.error("Error:", err);
 });
 
-// // Adding data to the sorted set (just for demonstration)
-// redisClient.zadd("mySortedSet", 100, "member1");
-// redisClient.zadd("mySortedSet", 90, "member2");
-// redisClient.zadd("mySortedSet", 80, "member3");
-// // Add more members with their respective scores
-
-// // Retrieve the top 10 elements with the highest scores using zrevrange
-// redisClient.zrevrange(
-//   "mySortedSet",
-//   0,
-//   9,
-//   "WITHSCORES",
-//   (err, membersWithScores) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       // The result will be an array alternating between member and score [member1, score1, member2, score2, ...]
-//       console.log("Top 10 elements with highest scores:");
-//       console.log(membersWithScores);
-//     }
-//   }
-// );
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`SSP Server Listening on PORT ${PORT}`);
+});
